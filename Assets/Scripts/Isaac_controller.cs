8da9079c26bda6dbb49be87bf4c88212653b338e
Jahir Larico <laricovargasjahir444@gmail.com>
2026-09-08 16:53:05 -0500

    Avance animaciones y funciones

--- Assets/Scripts/Isaac_controller.cs
+++ Assets/Scripts/Isaac_controller.cs
@@ -6,11 +6,16 @@ public class Isaac_controller : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    public int speed = 5;
-
+    public int speed = 2;
+    public int jumpForce = 200;
     private bool spriteRight = true;
     private float HorizontalMove;
     private float VerticalMove;
+    public bool inGroud = true;
+    public Animator animator;
+    public Transform feetPos;
+    public float checkRaduis = 5;
+    public LayerMask whatIsUnder;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,13 +26,23 @@ public class Isaac_controller : MonoBehaviour
         float HorizontalMove = Input.GetAxis("Horizontal");
         float VerticalMove = Input.GetAxis("Vertical");
         rb.linearVelocity = new Vector2(HorizontalMove * speed, rb.linearVelocity.y);
+        animator.SetFloat("Speed",Math.Abs(HorizontalMove));
     }
     // Update is called once per frame
     void Update()
     {
         float HorizontalMove = Input.GetAxis("Horizontal");
+        inGroud = Physics2D.OverlapCircle(feetPos.position, checkRaduis, whatIsUnder);
+        Debug.Log(inGroud);
         if ((HorizontalMove < 0.0f && spriteRight) || (HorizontalMove > 0.0f && !spriteRight)) {
             FlipIsaac();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Se presiono la letrea A");
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && inGroud) {
+            Jump();
         }
     }
 
@@ -36,5 +51,26 @@ public class Isaac_controller : MonoBehaviour
         Vector2 isaccScale = gameObject.transform.localScale;
         isaccScale.x *= -1;
         transform.localScale = isaccScale;
+    }
+
+    void Jump() {
+        rb.AddForceY(jumpForce);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("toco el piso");
+
+        if (collision.gameObject.tag == "Ground") {
+            inGroud = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("dejo el piso");
+        if (collision.gameObject.tag == "Ground")
+        {
+            inGroud = false;
+        }
     }
 }
