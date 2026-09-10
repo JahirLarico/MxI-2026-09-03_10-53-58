2b226784ea51478a1c9d9c97664e4cc3fb6fa9ea
Jahir Larico <laricovargasjahir444@gmail.com>
2026-09-10 12:52:33 -0500

    Avance codigo enemigos

--- Assets/Scripts/Gazling_controller.cs
+++ Assets/Scripts/Gazling_controller.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Gazling_controller : MonoBehaviour
 {
+
+    public float speed = 0.6f;
+    public bool moveRight = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,32 @@ public class Gazling_controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (moveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Pipe" || collision.gameObject.tag == "Enemie") {
+            if (moveRight) { 
+                moveRight = false;
+            }
+            else {
+                moveRight = true;
+            }
+
+        }
+
+        if (collision.gameObject.tag == "Player") {
+            if (transform.position.y < collision.transform.position.y) {
+                Destroy(gameObject);
+            } 
+        }
     }
 }
